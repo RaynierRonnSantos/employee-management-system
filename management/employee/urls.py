95d387a2e3d7f307ccd4498efdb61dfc37df9b56
RaynierRonnSantos <rayniersantos0404@gmail.com>
2025-03-08 11:36:58 +0800

    Initial commit

--- management/employee/urls.py
+++ management/employee/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import EmployeeViewSet, SalaryViewSet, PerformanceViewSet
+from .views import EmployeeViewSet, SalaryViewSet, PerformanceViewSet, AttendanceViewSet
 
 router = DefaultRouter()
 router.register(r'employees', EmployeeViewSet, basename='employee')
 router.register(r'salary', SalaryViewSet, basename='salary')
 router.register(r'performance', PerformanceViewSet, basename='performance')
+router.register(r'attendance', AttendanceViewSet, basename='attendance')
 
 urlpatterns = [
     path('', include(router.urls)),
