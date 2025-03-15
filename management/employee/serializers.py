f8590f006923078c73434e5dfdc8a94e0eab6f9a
RaynierRonnSantos <rayniersantos0404@gmail.com>
2025-03-15 10:38:54 +0800

    ini

--- management/employee/serializers.py
+++ management/employee/serializers.py
@@ -5,11 +5,14 @@ class EmployeeSerializer(serializers.ModelSerializer):
     name = serializers.CharField(required=True)
     department = serializers.CharField(required=True)
     position = serializers.CharField(required=True)
-    salary = serializers.DecimalField(max_digits=20, decimal_places=2, required=True)
+    salary = serializers.SerializerMethodField()
 
     class Meta:
         model = Employee
         fields = '__all__'
+
+    def get_salary(self, obj):
+        return f"₱{obj.salary:,.2f}"
 
     def validate_salary(self, value):
         if value <= 0:
@@ -18,11 +21,18 @@ class EmployeeSerializer(serializers.ModelSerializer):
 
 class SalaryHistorySerializer(serializers.ModelSerializer):
     employee_name = serializers.CharField(source='employee.name', read_only=True)
-    new_salary = serializers.DecimalField(max_digits=20, decimal_places=2)
+    previous_salary = serializers.SerializerMethodField()
+    new_salary = serializers.SerializerMethodField()
 
     class Meta:
         model = SalaryHistory
         fields = '__all__'
+
+    def get_previous_salary(self, obj):
+        return f"₱{obj.new_salary:,.2f}"
+
+    def get_new_salary(self, obj):
+        return f"₱{obj.new_salary:,.2f}"
 
     def validate_new_salary(self, value):
         if value <= 0:
